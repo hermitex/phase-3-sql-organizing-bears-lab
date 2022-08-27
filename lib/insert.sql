44b023b81fc6f7b392787d3b7c44f980ca8f55f7
hermitex <mercytibs@yahoo.com>
2022-08-27 19:24:36 +0300

    commit tree f153f2b86ca7f19c224854220bb1d7242791953d

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears(id,name, age, sex, color, temperament, alive) 
+VALUES 
+    (1, "Mr. Chocolate", 3, "M", "Red", "Hash", 1),
+    (2, "Rowdy", 3, "M", "Red", "Hash", 1),
+    (3, "Tabitha", 3, "F", "Red", "Hash", 1),
+    (4, "Sergeant Brown", 3, "M", "Red", "Hash", 1),
+    (5, "Melissa", 3, "M", "Red", "Hash", 1),
+    (6, "Grinch", 3, "F", "Red", "Hash", 1),
+    (7, "Wendy", 3, "M", "Red", "Hash", 1),
+    (8, NULL, 3, "F", "Red", "Hash", 1)
+  
